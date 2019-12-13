03d396c7c71c424b093728cc8f68896502e95ccd
khanhluong1 <derick.luong@gmail.com>
2019-12-13 11:52:07 -0500

    todo

---
+++ src/main/java/com/example/demo/Excercise303Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Excercise303Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Excercise303Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/demo/Todo.java
@@ -0,0 +1,58 @@
+package com.example.demo;
+
+import com.sun.istack.NotNull;
+import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min=3)
+    private String taskName;
+
+    @NotNull
+    private String dueDate;
+
+    @NotNull
+    private String priority;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+}
